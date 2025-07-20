9a044f1be99c115de561df53545c36f1fc19ff00
Malki Yasara <258165632+malkiyasara@users.noreply.github.com>
2025-07-20 17:45:07 +0530

    feat(main): implement main method

---
+++ src/main/java/com/sims/SIMSApplication.java
@@ -0,0 +1,13 @@
+package com.sims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SIMSApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SIMSApplication.class, args);
+    }
+}
+
